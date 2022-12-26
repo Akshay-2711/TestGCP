6dc6978689c5b535c579f6079ae80581621e6b38
63428_Akshay <akshaygavhane8499@gmail.com>
2022-12-26 17:56:11 +0530

    server

---
+++ login page/server.js
@@ -0,0 +1,10 @@
+var express=require("require");
+var app=express();
+var url=require("url");
+var fs=require("fs");
+
+app.get((req,res)=>{
+    res.sendFile("./log.html",{root:__dirname});
+});
+app.listen(8888);
+console.log("Server is listening at port 8888");
